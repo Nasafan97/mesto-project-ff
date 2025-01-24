80ddda944f0bdc755eb07944e84e32defd5d1223
Anastasia Afanaseva <nasafan97@yandex.ru>
2025-01-24 15:16:09 +0700

    Проект-5.Версия-2

--- scripts/index.js
+++ scripts/index.js
@@ -7,26 +7,31 @@ const cardContent = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 function createCard (card) {
     cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__image').alt = card.name;
-    cardElement.querySelector('.card__title').textContent = card.name;
+    cardImageLink = cardElement.querySelector('.card__image');
+    cardImageAlt =cardElement.querySelector('.card__image');
+    cardTitle = cardElement.querySelector('.card__title');
+
+    cardImageLink.src = card.link;
+    cardImageAlt.alt = card.name;
+    cardTitle.textContent = card.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', deleteCard);
 
-    cardContent.append(cardElement);
+    return cardElement;
 }
 
 // @todo: Функция удаления карточки
 function deleteCard(card) {
     const cardElement = card.target.closest('.card');
     cardElement.remove();
+    // card.remove();  На самом деле я сначала пробовала этот вариант, но тогда вообще ничего не удалялось, потом удалялся только последний элемент, причем при нажатии на любую кнопку "удалить" и я не понимаю как это исправить.
 }
 
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((elem) => {
-    createCard(elem, deleteCard);
+    cardContent.append(createCard(elem));
 })
 
 
