9831b49a23e4728a18690ea7c4eaa3dfa42d8b0d
Anastasia Afanaseva <nasafan97@yandex.ru>
2025-01-25 16:48:36 +0700

    Проект-5.Версия-3

--- scripts/index.js
+++ scripts/index.js
@@ -6,26 +6,23 @@ const cardContent = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard (card) {
-    cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    cardImageLink = cardElement.querySelector('.card__image');
-    cardImageAlt =cardElement.querySelector('.card__image');
-    cardTitle = cardElement.querySelector('.card__title');
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
 
-    cardImageLink.src = card.link;
-    cardImageAlt.alt = card.name;
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
     cardTitle.textContent = card.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', deleteCard);
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
 
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
 function deleteCard(card) {
-    const cardElement = card.target.closest('.card');
-    cardElement.remove();
-    // card.remove();  На самом деле я сначала пробовала этот вариант, но тогда вообще ничего не удалялось, потом удалялся только последний элемент, причем при нажатии на любую кнопку "удалить" и я не понимаю как это исправить.
+    card.remove();  // Большое большое спасибо за наставления!
 }
 
 
